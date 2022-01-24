be4fe882d13dce503691997ac71723b064573312
d-ava <d.avalishvili@gmail.com>
2022-01-24 20:30:55 +0400

    todo list updated

--- app/src/main/java/com/example/baadbank/todo.kt
+++ app/src/main/java/com/example/baadbank/todo.kt
@@ -2,26 +2,30 @@ package com.example.baadbank
 
 //progress bar - დათო
 //Todo cursor color - დათო <- better change edittext to material edit text
-//Todo status bar color
+//status bar color
 //calculator input type დათო
 //string to resources - ანა
-//Todo dimensions
-//Todo Language change
-//Todo Remember me - ანა
-//TOdo change app's launch logo - ანა
+//Todo dimensions - ანა
+
+//Remember me - ანა
+
 //arithmetic notations - დათო
 //savings minus value - დათო
 //register success loading - დათო
 //Todo continue as guest looking cool - დათო
-//Todo currency fragment UI
-//Todo convert fragment make it easy
-//Todo crypto item chain
+//Todo Welcome screen - ანა
+//Todo currency fragment UI - დათო.  ყიდვა გაყიდვა. API
+//convert fragment make it easy
+//Todo crypto item chain - დათო
 //savings ballance 322.77  <--
-//Todo user info implement avatar API
-//Todo user info avatar from device
-//Todo OFFLINE MODE firebase
+
+//Todo user info avatar from device - ანა ?
+
 //turn off orientation change - დათო
+
 //Todo unit test
-//Todo dark mode - დათო
-//Todo back press   error. stack problems with navigation
+
+//Todo back press   error. stack problems with navigation - ანა
+//Todo load during splash screen - დათო
+//Todo App Logo დათო
 
