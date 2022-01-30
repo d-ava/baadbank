16787b5ff3c75dddb60b81e9bc924d59f5d28ec8
d-ava <d.avalishvili@gmail.com>
2022-01-30 21:11:31 +0400

    proper names. event class constnt class interface from CryptoRepositoryImpl. FakeCryptoRepository

---
+++ app/src/main/java/com/example/baadbank/repository/CryptoRepositoryImpl.kt
@@ -0,0 +1,43 @@
+package com.example.baadbank.repository
+
+import com.example.baadbank.data.CoinGecko
+import com.example.baadbank.network.CoinGeckoApi
+import com.example.baadbank.util.Resource
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import java.io.IOException
+import javax.inject.Inject
+
+class CryptoRepositoryImpl @Inject constructor(private val coinApi: CoinGeckoApi) : CryptoRepository{
+
+     override fun getCoins(): Flow<Resource<List<CoinGecko>>>{
+        return flow{
+            try {
+                emit(Resource.Loading())
+                val response = coinApi.getCoinGecko()
+                val body = response.body()
+                if (response.isSuccessful && body != null){
+                    emit(Resource.Success(body))
+            }else{
+                emit(Resource.Error("error from repository"))
+            }
+
+
+            }catch (e:IOException){
+                emit(Resource.Error(e.message?: "error message"))
+            }
+
+        }.flowOn(IO)
+
+    }
+
+
+
+
+
+
+
+
+}
